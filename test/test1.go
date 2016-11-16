07a6e9ebf42f5b76ddecae08cba5e74441233fd0
cjj <cjj_vachel@163.com>
2016-11-16 15:02:28 +0800

    Changes to be committed: 	new file:   test/test1.go

---
+++ test/test1.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+)
+
+// Test1Chaincode aaa
+type Test1Chaincode struct {
+}
+
+func main() {
+	err := shim.Start(new(Test1Chaincode))
+	if err != nil {
+		fmt.Printf("Error starting Test1 chaincode: %s", err)
+	}
+}
+
+// Init resets all the things
+func (t *Test1Chaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+
+	var userColDefs []*shim.ColumnDefinition
+	userColDefs[0] = &shim.ColumnDefinition{"id", shim.ColumnDefinition_STRING, true}
+	userColDefs[1] = &shim.ColumnDefinition{"phoneNo", shim.ColumnDefinition_STRING, false}
+	err := stub.CreateTable("mg_user", userColDefs)
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+// Invoke isur entry point to invoke a chaincode function
+func (t *Test1Chaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Println("invoke is running " + function)
+
+	// Handle different functions
+	if function == "init" {
+		return t.Init(stub, "init", args)
+	} else if function == "insert" {
+		var row shim.Row
+		row.Columns[0] = &shim.Column{&shim.Column_String_{"1001"}}
+		row.Columns[1] = &shim.Column{&shim.Column_String_{"13401055240"}}
+		stub.InsertRow("mg_user", row)
+		return nil, nil
+	}
+	fmt.Println("invoke did not find func: " + function)
+
+	return nil, errors.New("Received unknown function invocation: " + function)
+}
+
+// Query aa
+func (t *Test1Chaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Println("query is running " + function)
+
+	// Handle different functions
+	if function == "read" { //read a variable
+		var key []shim.Column
+		key[0] = shim.Column{&shim.Column_String_{"1001"}}
+		row, err := stub.GetRow("mg_user", key)
+		return []byte(row.String()), err
+	}
+	fmt.Println("query did not find func: " + function)
+
+	return nil, errors.New("Received unknown function query: " + function)
+}
